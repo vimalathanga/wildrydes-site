ce4e5e9638cf6740eef5b45a358e541e9d57c239
vimalathanga <110455282+vimalathanga@users.noreply.github.com>
2024-11-28 09:11:57 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_zDtMk7OT9 ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1hopkrlhu9dv5l9frtjb5qhtsn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
